ab906533f32b828d5db5795ccea1dc445c6c5a46
mdasadur5027 <mdasadur5027@gmail.com>
2025-07-10 02:56:29 +0600

    first commit

---
+++ electron.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = process.env.ELECTRON_IS_DEV === 'true';
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1200,
+    minHeight: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    icon: path.join(__dirname, 'icon.png'),
+    titleBarStyle: 'default',
+    show: false
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
